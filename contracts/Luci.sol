2c729f6c26ea909eba5ca130dba31abba5255a19
mrspiggot <walker.richard.james@gmail.com>
2022-09-01 09:42:45 +0100

    compiled contact with brownie

--- contracts/Luci.sol
+++ contracts/Luci.sol
@@ -1,7 +1,11 @@
+// SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.0;
 
-contract Luci {
-    function Luci(){
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+contract Luci is ERC20 {
+    constructor (address _founder, uint256 _initialSupply) ERC20("Lucidate","LUCI"){
+        _mint(_founder,_initialSupply);
     }
 }
---
+++ scripts/deploy.py
@@ -0,0 +1,56 @@
+from brownie import Luci, accounts, QuizReward, network, config
+from web3 import Web3
+
+LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local"]
+
+
+def get_accounts():
+    print(f"get_account network is {network.show_active()}")
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        print(f"Network is {network.show_active()}")
+        print(accounts[0], len(accounts))
+        return accounts[0], accounts[1], accounts[2]
+    else:
+        print(f"Network is {network.show_active()}")
+        return accounts.add(config["wallets"]["from_key"]), accounts.add(config["wallets"]["from_key2"]), accounts.add(
+            config["wallets"]["from_key3"])
+
+
+def deploy_token():
+    initial_supply = Web3.toWei(21_000_000, "ether")
+    account, account2, account3 = get_accounts()
+
+    my_token = Luci.deploy(account, initial_supply, {"from": account},
+                                publish_source=config["networks"][network.show_active()].get("verify"))
+    return my_token
+
+
+def main():
+    my_token = deploy_token()
+
+    account_0, account_1, account_2 = get_accounts()
+    get_token_balance(account_0.address)
+
+    send_token(account_1, Web3.toWei(7_000_000, "ether"))
+    get_token_balance(account_0.address)
+    get_token_balance(account_1.address)
+    get_token_balance(account_2.address)
+
+
+
+def get_token_balance(account_address):
+    my_token = Luci[-1]
+    print(
+        f"The account {account_address} has balance {my_token.balanceOf(account_address)} of {my_token.symbol()} called {my_token.name()}")
+
+
+def send_token(receiver_address, amount):
+    my_token = Luci[-1]
+    my_token.transfer(receiver_address, amount)
+    get_token_balance(receiver_address)
+
+
+def set_allowance(spender_address, amount):
+    my_token = Luci[-1]
+    my_token.increaseAllowance(spender_address, amount)
+
